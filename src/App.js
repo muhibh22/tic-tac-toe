69a11291ecd779655c18adf879ea2743f6fe719c
Md. Mohibul Hasan <md.mohibul.hasan@g.bracu.ac.bd>
2023-09-18 00:05:21 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -23,6 +23,8 @@ function App() {
 
   const [board, setBoard]= useState(Array(9).fill(null) );
   const[xPlaying, setXPlaying]= useState(true);
+  const [scores, setScores]=useState({xScore:0, oScore:0});
+
   const handleBoxClick = (boxIdx) =>{
     const updatedBoard = board.map((value, idx) =>{
       if(idx==boxIdx){
@@ -33,7 +35,22 @@ function App() {
       }
     })
 
-    checkWinner(updatedBoard);
+    const winner= checkWinner(updatedBoard);
+
+    if (winner){
+      if(winner==="O"){
+        let {oScore}=scores;
+        oScore+=1;
+        setScores({...scores, oScore})
+      }
+      else{
+        let {xScore}=scores;
+        xScore+=1;
+        setScores({...scores, xScore})
+      }
+    }
+
+    console.log(scores);
     setBoard(updatedBoard);
 
     setXPlaying(!xPlaying);
