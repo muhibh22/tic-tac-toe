11c4cd2f9a76babb19c748f2c2e43c8089f518c7
Md. Mohibul Hasan <md.mohibul.hasan@g.bracu.ac.bd>
2023-09-17 20:05:17 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Box } from "./components/Box.js";
 function App() {
+  const board = ["X", "X", "X", "X", "X", "X", "X", "X"];
   return (
     <div className="App">
       <Box value ="X" onClick={null}/>
