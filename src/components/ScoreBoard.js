c52769b35487bb0ed0c84d21c010df65bcabffd1
Md. Mohibul Hasan <md.mohibul.hasan@g.bracu.ac.bd>
2023-09-18 16:40:22 +0600

    commit

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -8,7 +8,7 @@ export const ScoreBoard = ({scores, xPlaying}) => {
   return (
     <div className="scoreboard">
     <span className={'score x-score ${!xPlaying && "inactive"}'}> X - {xScore}</span>
-    <span className={'score x-score ${xPlaying && "inactive"}'}> O - {oScore}</span>
+    <span className={'score o-score ${xPlaying && "inactive"}'}> O - {oScore}</span>
     </div>
   )
 }
