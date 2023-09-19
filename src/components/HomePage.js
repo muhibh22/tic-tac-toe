1a356ce3d26962fbfdb5de8b42019e4a7a4984c0
Md. Mohibul Hasan <md.mohibul.hasan@g.bracu.ac.bd>
2023-09-19 16:03:30 +0600

    commit

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,14 +1,19 @@
 // HomePage.js
 import React from 'react';
 import './HomePage.css';
+import ticTacToeImage from './3x3.png'; // Import your image
 
 function HomePage({ onSelectGameMode }) {
   return (
     <div className="home-page">
-      <h1>Welcome to Tic Tac Toe</h1>
+      <h1 className="home-title">Welcome to Tic Tac Toe</h1>
       <div className="button-container">
         <button className="game-button" onClick={() => onSelectGameMode(3)}>3x3 Match</button>
         <button className="game-button" onClick={() => onSelectGameMode(6)}>6x6 Match</button>
+      </div>
+      <div className="image-container">
+        <img src={ticTacToeImage} alt="Tic Tac Toe" className="game-image" />
+        <img src={ticTacToeImage} alt="Tic Tac Toe" className="game-image" />
       </div>
     </div>
   );
